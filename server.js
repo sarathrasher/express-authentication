928a4942e78aaf8f13750dca2fb3d22d18077cda
Sara Muntean <munteansr@gmail.com>
2018-08-24 13:03:46 -0400

    Added basic authentication functionality using Express.

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+
+let app = express();
+
+let users = [
+    {userId: '1', user: 'sara', email: 'sara@bobeara.com', password: 'pass1'},
+    {userId: '2', user: 'hannah', email: 'hannah@banana.com', password: 'pass2'},
+    {userId: '3', user: 'billy', email: 'billy@bob.com', password: 'pass3'}
+]
+
+let squabs = [
+    {squabId: '1', userId: '1', squabs: 'I hate you'},
+    {squabId: '3', userId: '2', squabs: 'Blurgh"'},
+    {squabId: '3', userId: '3', squabs: 'This stinks.'}
+];
+
+let authenticate = (req, res, cb) => {
+    users.forEach((user) => {
+        if (req.query.email === user.email && req.query.password === user.password) {
+            cb();
+        }
+    });
+    res.end('Not Allowed.');
+};
+
+let listUsers = (req, res) => {
+    res.send(users);
+};
+
+let listUserSquabs = (req, res) => {
+    let userId = req.params.userId;
+    console.log(userId);
+    let mySquab = squabs.filter(squab =>
+        squab.userId === userId
+    )
+    res.send(mySquab);
+}
+
+let listUser = (req, res) => {
+    let userId = req.params.userId;
+    let user = users.filter(user => 
+        user.userId === userId
+    );
+    res.send(user);
+}
+
+app.get('/users/:userId/squabs', authenticate, listUserSquabs)
+app.get('/users/:userId/', authenticate, listUser)
+app.get('/users', authenticate, listUsers);
+
+
+app.listen(3000);
